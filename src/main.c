d03debe36a0c6fd0d869bf091a96a044148fe2a7
OpenAcousticDevices <theteam@openacousticdevices.info>
2018-09-21 12:56:29 +0100

    Added firmware version and description.

--- src/main.c
+++ src/main.c
@@ -8,13 +8,34 @@
 
 #include "audioMoth.h"
 
+/* Firmware version and description */
+
+static uint8_t firmwareVersion[AM_FIRMWARE_VERSION_LENGTH] = {1, 0, 0};
+
+static uint8_t firmwareDescription[AM_FIRMWARE_DESCRIPTION_LENGTH] = "Example-Firmware";
+
 /* Required interrupt handles */
 
-void AudioMoth_handleSwitchInterrupt(void) { };
-void AudioMoth_handleMicrophoneInterrupt(int16_t sample) { };
-void AudioMoth_handleDirectMemoryAccessInterrupt(bool primaryChannel, int16_t **nextBuffer) { };
-void AudioMoth_usbApplicationPacketRequested(uint32_t messageType, uint8_t *transmitBuffer, uint32_t size) { };
-void AudioMoth_usbApplicationPacketReceived(uint32_t messageType, uint8_t *receiveBuffer, uint8_t *transmitBuffer, uint32_t size) { };
+void AudioMoth_handleSwitchInterrupt(void) { }
+void AudioMoth_handleMicrophoneInterrupt(int16_t sample) { }
+void AudioMoth_handleDirectMemoryAccessInterrupt(bool primaryChannel, int16_t **nextBuffer) { }
+
+/* Required USB message handlers */
+
+void AudioMoth_usbFirmwareVersionRequested(uint8_t **firmwareVersionPtr) {
+
+    *firmwareVersionPtr = firmwareVersion;
+
+}
+
+void AudioMoth_usbFirmwareDescriptionRequested(uint8_t **firmwareDescriptionPtr) {
+
+    *firmwareDescriptionPtr = firmwareDescription;
+
+}
+
+void AudioMoth_usbApplicationPacketRequested(uint32_t messageType, uint8_t *transmitBuffer, uint32_t size) { }
+void AudioMoth_usbApplicationPacketReceived(uint32_t messageType, uint8_t *receiveBuffer, uint8_t *transmitBuffer, uint32_t size) { }
 
 /* Main function */
 
