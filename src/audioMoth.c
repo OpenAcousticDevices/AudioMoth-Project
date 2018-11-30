2b02edc7ade1fc00a72e1594924df96630eb6b0f
Alex Rogers <31531807+AlexRogersCS@users.noreply.github.com>
2018-11-30 23:41:39 +0000

    Remove extra debugging output declaration

--- src/audioMoth.c
+++ src/audioMoth.c
@@ -1810,7 +1810,7 @@ static void setupGPIO(void) {
 
 }
 
-/* Enable SWO output for debugging */
+/* Enable SWO output for debugging output */
 
 int _write(int file, const char *ptr, int len) {
 
@@ -1819,50 +1819,6 @@ int _write(int file, const char *ptr, int len) {
     for (x = 0; x < len; x++) ITM_SendChar (*ptr++);
 
     return (len);
-
-}
-
-void AudioMoth_setUpDebugOutput(void) {
-
-    /* Enable GPIO clock. */
-
-    CMU ->HFPERCLKEN0 |= CMU_HFPERCLKEN0_GPIO;
-
-    /* Enable Serial wire output pin */
-
-    GPIO ->ROUTE |= GPIO_ROUTE_SWOPEN;
-
-    /* Set location 0 */
-
-    GPIO ->ROUTE = (GPIO ->ROUTE & ~(_GPIO_ROUTE_SWLOCATION_MASK)) | GPIO_ROUTE_SWLOCATION_LOC0;
-
-    /* Enable output on pin - GPIO Port F, Pin 2 */
-
-    GPIO ->P[5].MODEL &= ~(_GPIO_P_MODEL_MODE2_MASK);
-    GPIO ->P[5].MODEL |= GPIO_P_MODEL_MODE2_PUSHPULL;
-
-    /* Enable debug clock AUXHFRCO */
-
-    CMU ->OSCENCMD = CMU_OSCENCMD_AUXHFRCOEN;
-
-    /* Wait until clock is ready */
-
-    while (!(CMU ->STATUS & CMU_STATUS_AUXHFRCORDY)) ;
-
-    /* Enable trace in core debug */
-
-    CoreDebug ->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
-
-    ITM ->LAR = 0xC5ACCE55;
-    ITM ->TER = 0x0;
-    ITM ->TCR = 0x0;
-    TPI ->SPPR = 2;
-    TPI ->ACPR = 0xf;
-    ITM ->TPR = 0x0;
-    DWT ->CTRL = 0x400003FE;
-    ITM ->TCR = 0x0001000D;
-    TPI ->FFCR = 0x00000100;
-    ITM ->TER = 0x1;
 
 }
 
