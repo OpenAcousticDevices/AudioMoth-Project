b48c058a8d8e78f2bf3eca9154b0b0aa9fb1c3be
Alex Rogers <31531807+AlexRogersCS@users.noreply.github.com>
2024-05-09 14:53:26 +0100

    Update to add dawn and dusk calculations.

--- gps/inc/sunrise.h
+++ gps/inc/sunrise.h
@@ -7,8 +7,14 @@
 #ifndef __SUNRISE_H
 #define __SUNRISE_H
 
-bool Sunrise_calculateFromDate(uint32_t year, uint32_t month, uint32_t day, float latitude, float longitude, uint32_t *sunriseMinutes, uint32_t *sunsetMinutes);
+typedef enum {SR_SUN_ABOVE_HORIZON, SR_NORMAL_SOLUTION, SR_SUN_BELOW_HORIZON} SR_solution_t;
 
-bool Sunrise_calculateFromUnix(uint32_t currentTime, float latitude, float longitude, uint32_t *sunriseMinutes, uint32_t *sunsetMinutes);
+typedef enum {SR_DAY_LONGER_THAN_NIGHT, SR_DAY_EQUAL_TO_NIGHT, SR_DAY_SHORTER_THAN_NIGHT} SR_trend_t;
+
+typedef enum {SR_SUNRISE_AND_SUNSET, SR_CIVIL_DAWN_AND_DUSK, SR_NAUTICAL_DAWN_AND_DUSK, SR_ASTRONOMICAL_DAWN_AND_DUSK} SR_event_t;
+
+void Sunrise_calculateFromDate(SR_event_t event, uint32_t year, uint32_t month, uint32_t day, float latitude, float longitude, SR_solution_t *solution, SR_trend_t *trend, uint32_t *sunriseMinutes, uint32_t *sunsetMinutes);
+
+void Sunrise_calculateFromUnix(SR_event_t event, uint32_t currentTime, float latitude, float longitude, SR_solution_t *solution, SR_trend_t *trend, uint32_t *sunriseMinutes, uint32_t *sunsetMinutes);
 
 #endif /* __SUNRISE_H */
